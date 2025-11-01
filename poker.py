06daeffc8f3bc369692aaba9225d1e42e08b2273
Henry Barlow <151530813+henrybarl0w@users.noreply.github.com>
2025-11-01 13:21:36 +0000

    start implemetnign deal fucntion

--- poker.py
+++ poker.py
@@ -1,5 +1,5 @@
 # Durhack 2025
-
+import random
 # Tempory function to generate deck
 '''suits = ['H','S','D','C']
 values = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
@@ -9,11 +9,25 @@ for suit in suits:
         deck.append(value + suit)
 print(deck)'''
 
-# Hard codded???
-deck = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC']
+class Dealer():
+    def __init__(self):
+        self.deck = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH', 
+                '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS', 
+                '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD', 
+                '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC']
+        self.players = []
+        self.communityCards = []
 
-print(deck.pop())
-print(deck)
+    # Deal two cards to every player
+    def deal(self):
+        random.shuffle(self.deck)
+        for player in self.players:
+            player.giveCard()
+        pass
+
+    # Deals with a round of betting and shows n number of cards at the start
+    def betting(self, n):
+        pass
 
 class Player():
     def __init__(self):
