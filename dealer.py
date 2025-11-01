a11f3f1949fd4fd7a04eb5fdc8146ed2ea86d338
Henry Barlow <151530813+henrybarl0w@users.noreply.github.com>
2025-11-01 15:22:58 +0000

    draft of betting function

--- dealer.py
+++ dealer.py
@@ -9,6 +9,7 @@ class Dealer():
         self.players = []
         self.communityCards = []
         self.little = 0
+        self.minBet = 5
 
     def reset(self):
         for player in self.players:
@@ -18,6 +19,7 @@ class Dealer():
             '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS', 
             '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD', 
             '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC']
+        self.minBet = 5
 
     # Deal two cards to every player
     def deal(self):
@@ -27,12 +29,19 @@ class Dealer():
 
     # Deals with a round of betting and shows n number of cards at the start
     def betting(self, n):
-        minBet = 0
         for i in range(n):
             self.communityCards.append(self.deck.pop())
-        for i in range(self.little, self.little+len(self.players)):
-            j = i % len(self.players)
-            player = self.players[j]
-        if not player.isFolded(): betSize = player.bet(minBet)
-        if betSize == -1: player.fold()
-        elif betSize > minBet: minBet = betSize
+        #for i in range(self.little, self.little+len(self.players)):
+        count = 0
+        index = self.little
+        while index < self.little+len(self.players) and count != len(self.players) - 1:
+            player = self.players[index % len(self.players)]
+            if not player.isFolded(): 
+                betSize = player.bet(self.minBet)
+            if betSize == -1: 
+                player.fold()
+            elif betSize > self.minBet: 
+                self.minBet = betSize
+                count = 0
+            elif betSize == self.minBet: 
+                count += 1
