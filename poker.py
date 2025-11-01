56b72d8dfac2c5f803c68c797a975d0975d57d9e
lxbv72 <237954879+lxbv72@users.noreply.github.com>
2025-11-01 13:53:07 +0000

    added a check function for Player

--- poker.py
+++ poker.py
@@ -33,19 +33,24 @@ class Player():
     def __init__(self):
         self.__cards = []
         self.__money = 0
+        self.__totalGameBet = 0
+
+    def gameReset(self):
+        self.__totalGameBet = 0
+        
 
     def giveCard(self, card):
         # method to give a card to a player
+        # input: 2-char string eg "2H"
 
         # ensure that the hand is not already full
-        if len(self.cards) >= 2:
+        if len(self.__cards) >= 2:
             raise Exception("Player hand is already full")
         
         # ensure the card is in the valid format, eg 2S, AQ
         if len(card) != 2:
             raise Exception("Please use the valid format for cards")
-        
-        if card[0] not in "23456789JQKA" or card[1] not in "HSDC":
+        if (card[0] not in "23456789JQKA") or (card[1] not in "HSDC"):
             raise Exception("Please use the valid format for cards")
         
         self.cards.append(card)
@@ -54,11 +59,16 @@ class Player():
         # method to return all private cards a player has in their hand
         return self.__cards
     
+    def clearCards(self):
+        self.__cards = []
+    
     def setMoney(self, nMoney):
         self.__money = nMoney
 
     def addMoney(self, nMoney):
         self.__money += nMoney
+
+    
 
     def isBankrupt(self):
         # function to be run at the start of the game for each players
@@ -66,3 +76,17 @@ class Player():
             return True
         return False
 
+    def check(self, currentPoolRequirement):
+        # input: current pool bet per person for this round
+        #output: True if check is met, False if it is not
+
+        bet_difference = currentPoolRequirement - self.__totalGameBet 
+        if bet_difference < 0:
+            raise Exception("The player has already bet more than is required for this round")
+
+        if self.__money <= bet_difference:
+            # Logic for going all in
+            return False
+    
+        self.__money = self.__money - bet_difference
+        return True
