3bc0bc0d6fd65835bfde9994212d5491a88e9d4f
lxbv72 <237954879+lxbv72@users.noreply.github.com>
2025-11-01 12:53:03 +0000

    modified:   gamefunctions.py

--- gamefunctions.py
+++ gamefunctions.py
@@ -1,1 +1,16 @@
 # game functions
+
+import random
+NCARDS = 52
+
+def shuffle_deck(d):
+    # function to shuffle the deck d
+    return random.shuffle(d)
+
+def deal(nPlayers, deck):
+
+    # check that the number of players is suitable for the number of cards
+    if nPlayers > (NCARDS-5)//2:
+        raise Exception("Too many players")
+    
+    
