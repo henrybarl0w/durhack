df464decad0b8d355a625b5d3fcd24e5512bb8ad
Henry Barlow <151530813+henrybarl0w@users.noreply.github.com>
2025-11-02 00:55:49 +0000

    accounted for none hands

--- src/dealer.py
+++ src/dealer.py
@@ -139,6 +139,8 @@ class Dealer():
         return h
 
     def rankHand(self, h):
+        if len(h) != 5:
+            return (0, 0) #'invalid hand'
         h = self.orderHand(h)
         cardOrder = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
         # Check for same suit
